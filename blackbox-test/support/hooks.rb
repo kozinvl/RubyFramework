eb9b944e6f817b73c7bda2428f8ee41be270b181
kozinvl <vladiksystem@gmail.com>
2020-05-10 18:06:14 +0300

    Refactoring

--- blackbox-test/support/hooks.rb
+++ blackbox-test/support/hooks.rb
@@ -1,14 +1,12 @@
-Before do |scenario|
-  puts "#{scenario.name}"
+Before do |test|
+  puts "Current scenario: #{test.feature.name}: #{test.name}"
 end
 
-After do |scenario|
-  scenario.source_tag_names.each do |tag|
-    if tag.include?('@web_ui') && scenario.failed?
-      screenshot_path = "test-reports/#{Time.now.to_i}.png"
-      web_app.page.save_screenshot(screenshot_path)
-      puts "  Screenshot: #{screenshot_path} saved"
-    end
+After('@web_ui') do |test|
+  if test.failed?
+    screenshot_path = "test-reports/#{test.name.gsub(' ', '_')}.png"
+    web_app.page.save_screenshot(screenshot_path)
+    puts "Screenshot can be found at #{screenshot_path}"
   end
 end
 
