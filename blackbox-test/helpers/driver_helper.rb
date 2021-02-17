6b08c8409795679859a245f90036713094edc6b1
kozinvl <vladiksystem@gmail.com>
2021-02-17 19:20:47 +0200

    Clean up [Driver Helper]

--- blackbox-test/helpers/driver_helper.rb
+++ blackbox-test/helpers/driver_helper.rb
@@ -3,26 +3,36 @@
 module DriverHelper
   module_function
 
-  def browser
-    ConfigHelper.instance[:browser]
-  end
-
   def register_browser
-    Capybara.register_driver :selenium_chrome do |app|
-      Capybara::Selenium::Driver.new(app,
-                                     browser: :remote,
-                                     url: "http://#{browser[:host]}:#{browser[:port]}/wd/hub",
-                                     desired_capabilities: browser_capabilities)
+    if browser_settings[:run_in_docker]
+      Capybara.register_driver :selenium_chrome do |app|
+        Capybara::Selenium::Driver.new(app,
+                                       browser_settings: :remote,
+                                       url: remote_browser_url,
+                                       desired_capabilities: browser_capabilities)
+      end
     end
-    Capybara.default_driver = browser[:name]
-    Capybara.current_driver = browser[:name]
-    Capybara.default_max_wait_time = browser[:default_max_wait_time]
+    Capybara.default_driver = browser_name
+    Capybara.current_driver = browser_name
+    Capybara.default_max_wait_time = browser_settings[:default_max_wait_time]
   end
 
   def browser_capabilities
     opts = { chromeOptions:
-               { args: browser[:capabilities] }
+               { args: browser_settings[:capabilities] }
     }
     Selenium::WebDriver::Remote::Capabilities.chrome(opts)
+  end
+
+  def browser_settings
+    ConfigHelper.instance[:browser_settings]
+  end
+
+  def remote_browser_url
+    "http://#{browser_settings[:host]}:#{browser_settings[:port]}/wd/hub"
+  end
+
+  def browser_name
+    browser_settings[:name]
   end
 end
