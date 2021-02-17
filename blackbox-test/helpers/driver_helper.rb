3f06a45e38e17fc4cfe7aa68e42d9b9538593d84
kozinvl <vladiksystem@gmail.com>
2021-02-17 18:35:23 +0200

    Config for Browser was created

--- blackbox-test/helpers/driver_helper.rb
+++ blackbox-test/helpers/driver_helper.rb
@@ -1,25 +1,26 @@
 module DriverHelper
   extend self
 
+  def browser_config
+    ConfigHelper.instance[:browser]
+  end
+
   def register_browser
     Capybara.register_driver :selenium_chrome do |app|
       Capybara::Selenium::Driver.new(app,
                                      browser: :remote,
-                                     url: "http://#{ENV['SELENIUM_HOST']}:#{ENV['SELENIUM_PORT']}/wd/hub",
+                                     url: "http://#{browser_config[:host]}:#{browser_config[:port]}/wd/hub",
                                      desired_capabilities: browser_capabilities)
     end
     Capybara.default_driver = :selenium_chrome
     Capybara.current_driver = :selenium_chrome
-    Capybara.default_max_wait_time = 30
+    Capybara.default_max_wait_time = browser_config[:default_max_wait_time]
   end
 
   def browser_capabilities
     opts = { chromeOptions:
-                { args:
-                     ['--window-size=1360,998',
-                      '--ignore-certificate-errors']
-                }
-           }
+               { args: browser_config[:capabilities] }
+    }
     Selenium::WebDriver::Remote::Capabilities.chrome(opts)
   end
 end
